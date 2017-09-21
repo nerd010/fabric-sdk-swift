12a6be85ef88eb18e57790c15d58fc2e2499c696
smallX <xieyu559@163.com>
2017-09-21 23:29:22 +0800

    add proto

---
+++ Sources/Proto/Peer/admin.pb.swift
@@ -0,0 +1,8 @@
+//
+//  admin.pb.swift
+//  fabric-sdk-swiftPackageDescription
+//
+//  Created by Richard on 2017/9/21.
+//
+
+import Foundation
---
+++ Sources/Proto/Peer/peer.pb.swift
@@ -0,0 +1,8 @@
+//
+//  peer.pb.swift
+//  fabric-sdk-swiftPackageDescription
+//
+//  Created by Richard on 2017/9/21.
+//
+
+import Foundation
